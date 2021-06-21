ebb3cda76c999a616115b7fe569f5ce4c25266dc
Lenny <lenny.donnez@gmail.com>
2021-06-21 18:40:21 +0200

    Dont show preview window on Windows

--- .config/nvim/lua/ld/plugins/fzf.lua
+++ .config/nvim/lua/ld/plugins/fzf.lua
@@ -5,7 +5,12 @@ vim.api.nvim_set_keymap('n', '<leader>g', ':GFiles<CR>', { noremap = true, silen
 vim.cmd[[let $FZF_DEFAULT_COMMAND = 'ag --hidden --skip-vcs-ignores -l -g ""']]
 
 vim.g.fzf_layout = { window = { width = 0.7, height = 0.6, yoffset = 0.1, xoffset = 0.5, border = "sharp" } }
-vim.g.fzf_preview_window = 'down:60%'
+
+if vim.fn.has('win32') == 0 then
+  vim.g.fzf_preview_window = 'down:60%'
+else
+  vim.g.fzf_preview_window = {}
+end
 
 vim.g.fzf_action = { ["ctrl-t"] = "tab split"; ["ctrl-x"] = "split", ["ctrl-v"] = "vsplit" }
 
