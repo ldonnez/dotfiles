980294a258fe8820ff602d00c79451cda4b07722
Lenny <lenny.donnez@gmail.com>
2021-07-11 02:36:19 +0200

    Enable tree sitter

--- .config/nvim/lua/ld/treesitter.lua
+++ .config/nvim/lua/ld/treesitter.lua
@@ -1,8 +1,21 @@
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = false
-  },
-  incremental_selection = {
-    enable = false
-  }
-}
+require("nvim-treesitter.configs").setup({
+    autopairs = { enable = true },
+    autotag = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    ensure_installed = {
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "tsx",
+      "lua",
+      "json",
+      "jsonc",
+      "yaml",
+    },
+    highlight = { enable = true, disable = { "yaml" } }
+  })
