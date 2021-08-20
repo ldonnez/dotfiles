d7c6984a4a43fac4d71f75ea0d56336400974dc8
Lenny <lenny.donnez@gmail.com>
2021-08-20 04:32:39 +0200

    Specifically enable/disable clients

--- .config/nvim/lua/ld/plugins/coq.lua
+++ .config/nvim/lua/ld/plugins/coq.lua
@@ -1,3 +1,14 @@
 vim.g.coq_settings = {
 	auto_start = "shut-up",
+	clients = {
+		buffers = {
+			enabled = true,
+		},
+		snippets = {
+			enabled = true,
+		},
+		tree_sitter = {
+			enabled = false,
+		},
+	},
 }
