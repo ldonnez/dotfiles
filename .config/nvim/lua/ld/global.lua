0a73a7cb54100d719a43e5007b12671ae8efc154
Lenny <lenny.donnez@gmail.com>
2021-08-25 12:37:37 +0200

    Add plugin installation path

--- .config/nvim/lua/ld/global.lua
+++ .config/nvim/lua/ld/global.lua
@@ -1,6 +1,7 @@
 local global = {}
 local home = vim.fn.getenv("HOME")
 local config = vim.fn.stdpath("config")
+local data = vim.fn.stdpath("data")
 
 function global:load_variables()
 	self.home_dir = home
@@ -9,6 +10,8 @@ function global:load_variables()
 	self.undo_dir = config .. "/undo"
 	self.swap_dir = config .. "/swap"
 	self.cache_dir = config .. "/cache"
+	self.data = data
+	self.plugin_installation_path = data .. "/site/pack/packer/start"
 	self.is_windows = vim.fn.has("win32") == 1
 	self.is_unix = vim.fn.has("unix") == 1
 	self.is_mac = vim.fn.has("mac") == 1
