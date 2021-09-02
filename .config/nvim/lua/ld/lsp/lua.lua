1ba564b616dbe649c1c3edd9b29fdf2ba30bb89e
Lenny <lenny.donnez@gmail.com>
2021-09-02 10:45:41 +0200

    Dont check for third party configuration
    Removes the annoying question 'Do you need to configure your work environment as `LÖVE`? lua languae server'

--- .config/nvim/lua/ld/lsp/lua.lua
+++ .config/nvim/lua/ld/lsp/lua.lua
@@ -38,6 +38,7 @@ require("lspconfig").sumneko_lua.setup({
 				globals = { "vim" },
 			},
 			workspace = {
+				checkThirdParty = false,
 				-- Make the server aware of Neovim runtime files
 				library = vim.api.nvim_get_runtime_file("", true),
 			},
