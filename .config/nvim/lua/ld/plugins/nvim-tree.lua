2345195283401ed15ced88788c6171585d840f1a
Lenny <lenny.donnez@gmail.com>
2021-10-03 13:49:55 +0200

    Add nvim-tree setup
    According to new nvim-tree api setup tree should be added after the old
    method of configuring settings.

--- .config/nvim/lua/ld/plugins/nvim-tree.lua
+++ .config/nvim/lua/ld/plugins/nvim-tree.lua
@@ -1,11 +1,17 @@
-vim.api.nvim_set_keymap("n", "<C-e>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
 vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_width_allow_resize = 1
-vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_show_icons = {
   ["git"] = 1,
   ["folders"] = 0,
   ["files"] = 0,
   ["folder_arrows"] = 0,
 }
+
+require("nvim-tree").setup({
+  lsp_diagnostics = false,
+  view = {
+    auto_resize = true,
+  },
+})
+
+vim.api.nvim_set_keymap("n", "<C-e>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
