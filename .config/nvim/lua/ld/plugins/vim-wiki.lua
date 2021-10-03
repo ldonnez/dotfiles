edcdd19f535f0ef0a4129d3cf2329c1127891d45
Lenny <lenny.donnez@gmail.com>
2021-10-03 13:50:58 +0200

    Add :Diary command
    Running :Diary will open the index page of vimwiki's diary and
    automatically generates new links

--- .config/nvim/lua/ld/plugins/vim-wiki.lua
+++ .config/nvim/lua/ld/plugins/vim-wiki.lua
@@ -10,3 +10,12 @@ vim.g.vimwiki_table_mappings = 0
 vim.g.vimwiki_table_auto_fmt = 0
 
 vim.api.nvim_set_keymap("n", "tt", "<Plug>VimwikiToggleListItem", { noremap = false })
+
+-- automatically update links on read diary
+vim.cmd([[
+command! Diary VimwikiDiaryIndex
+augroup vimwikigroup
+    autocmd!
+    autocmd BufRead,BufNewFile diary.wiki VimwikiDiaryGenerateLinks
+augroup end
+]])
