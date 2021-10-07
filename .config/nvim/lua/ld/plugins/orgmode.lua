7a36d764be9a4c5d5814091447a118b49a7c8440
Lenny <lenny.donnez@gmail.com>
2021-10-07 16:54:53 +0200

    Improve templates

--- .config/nvim/lua/ld/plugins/orgmode.lua
+++ .config/nvim/lua/ld/plugins/orgmode.lua
@@ -5,15 +5,15 @@ require("orgmode").setup({
   org_agenda_files = { orgDestination .. "/*", orgDestination .. "/**/*" },
   org_default_notes_file = orgDestination .. "/refile.org",
   org_agenda_templates = {
-    t = { description = "Task", template = "* TODO %?\n %u", target = orgDestination .. "/todos.org" },
+    t = { description = "Task", template = "\n* TODO %?\n %u", target = orgDestination .. "/todos.org" },
     j = {
       description = "Journal",
-      template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+      template = "\n** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
       target = orgDestination .. "/journal.org",
     },
     b = {
       description = "Braindump",
-      template = "\n*** %<%Y-%m-%d %H:%M> \n\n%?",
+      template = "\n** %U \n<%a>\n\n%?",
       target = orgDestination .. "/braindump.org",
     },
   },
