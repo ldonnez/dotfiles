2cba981b8717ca3dc0d4375c470a3fc3e9527a77
Lenny <lenny.donnez@gmail.com>
2021-10-08 18:03:12 +0200

    Fix breaking change
    https://github.com/hrsh7th/nvim-cmp/issues/231

--- .config/nvim/lua/ld/plugins/nvim-cmp.lua
+++ .config/nvim/lua/ld/plugins/nvim-cmp.lua
@@ -27,8 +27,8 @@ cmp.setup({
   },
   mapping = {
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-n>", "n")
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif vim.fn["vsnip#available"]() == 1 then
         feedkey("<Plug>(vsnip-expand-or-jump)", "")
       elseif has_words_before() then
@@ -41,8 +41,8 @@ cmp.setup({
       "s",
     }),
     ["<S-Tab>"] = cmp.mapping(function()
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-p>", "n")
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif vim.fn["vsnip#jumpable"](-1) == 1 then
         feedkey("<Plug>(vsnip-jump-prev)", "")
       end
