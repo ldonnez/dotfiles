4b6e15dbbec1343fb2f50e358f2423e9188a36af
Lenny <lenny.donnez@gmail.com>
2021-11-09 12:40:42 +0100

    Cleanup & lazy load plugins

--- .config/nvim/lua/ld/plugins/init.lua
+++ .config/nvim/lua/ld/plugins/init.lua
@@ -20,13 +20,49 @@ vim.api.nvim_exec(
 local use = require("packer").use
 require("packer").startup(function()
   use("wbthomason/packer.nvim")
-  use("tpope/vim-fugitive")
+
+  -- Easy Git integration
+  use({ "tpope/vim-fugitive", cmd = { "G", "Git" } })
+
+  -- Git signs on the left side of the numbers
+  use({
+    "lewis6991/gitsigns.nvim",
+    event = "BufRead",
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("ld.plugins.gitsigns")
+    end,
+  })
+
+  -- LSP
   use({
     "neovim/nvim-lspconfig",
     config = function()
       require("ld.lsp")
     end,
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      after = "nvim-lspconfig",
+    },
   })
+
+  -- Autocompletion
+  use({
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    after = "nvim-lspconfig",
+    requires = {
+      { "hrsh7th/cmp-path", after = "nvim-cmp" },
+      { "kristijanhusak/vim-dadbod-completion", after = "nvim-cmp", ft = "sql" },
+    },
+    config = function()
+      require("ld.plugins.nvim-cmp")
+    end,
+  })
+
+  -- Highlighting
   use({
     "nvim-treesitter/nvim-treesitter",
     event = "BufEnter",
@@ -36,36 +72,54 @@ require("packer").startup(function()
       require("ld.treesitter")
     end,
   })
+
+  -- Save sessions & restore buffers for projects
   use({
     "thaerkh/vim-workspace",
+    event = "BufEnter",
     config = function()
       require("ld.plugins.workspace")
     end,
   })
+
+  -- Easy HTML/JSX
   use({
     "mattn/emmet-vim",
+    keys = { { "n", "<C-y>" }, { "i", "<C-y>" } },
+    ft = { "javascript", "javascriptreact", "typescriptreact", "html", "typescript" },
     config = function()
       require("ld.plugins.emmet")
     end,
   })
-  use("jiangmiao/auto-pairs")
-  use("alvan/vim-closetag")
-  use("tpope/vim-surround")
-  use({
-    "itchyny/lightline.vim",
-    event = "BufEnter",
-    config = function()
-      require("ld.plugins.lightline")
-    end,
-  })
+
+  -- Auto pair parentheses, brackets, quotes...
+  use({ "jiangmiao/auto-pairs", event = "BufRead" })
+
+  -- Surround everything
+  use({ "tpope/vim-surround", event = "BufRead" })
+
+  -- Theme
   use({
     "arcticicestudio/nord-vim",
     config = function()
       require("ld.plugins.nord")
     end,
   })
+
+  -- Better statusline
+  use({
+    "itchyny/lightline.vim",
+    after = "nord-vim",
+    config = function()
+      require("ld.plugins.lightline")
+    end,
+  })
+
+  -- Fast fuzzy searching everything
   use({
     "junegunn/fzf.vim",
+    cmd = { "Files", "Buffers", "GFiles" },
+    keys = { { "n", "<C-p>" }, { "n", "<leader>b" }, { "n", "<leader>g" } },
     config = function()
       require("ld.plugins.fzf")
     end,
@@ -76,72 +130,74 @@ require("packer").startup(function()
       end,
     },
   })
-  use({
-    "lewis6991/gitsigns.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("ld.plugins.gitsigns")
-    end,
-  })
+
+  -- Nice database UI
   use({
     "kristijanhusak/vim-dadbod-ui",
     cmd = { "DBUI" },
     requires = {
-      "tpope/vim-dadbod",
+      { "tpope/vim-dadbod", after = "vim-dadbod-ui" },
     },
     config = function()
       require("ld.plugins.vim-dadbod-ui")
     end,
   })
-  use("pantharshit00/vim-prisma")
+
+  -- Prisma syntax highlighting
+  use({ "pantharshit00/vim-prisma", ft = "prisma" })
+
+  -- Create tables in markdown/org
   use({
     "dhruvasagar/vim-table-mode",
+    cmd = { "TableModeToggle" },
+    keys = { "n", "<Leader>tm" },
     config = function()
       require("ld.plugins.table-mode")
     end,
   })
+
+  -- Generic language server in lua
   use({
     "jose-elias-alvarez/null-ls.nvim",
+    keys = { "n", "<Leader>f" },
+    ft = { "lua", "typescript", "typescriptreact", "javascript", "javascriptreact", "json", "yaml" },
     config = function()
       require("ld.plugins.null-ls")
     end,
   })
-  use({ "jose-elias-alvarez/nvim-lsp-ts-utils", requires = { "nvim-lua/plenary.nvim" } })
+
+  -- Improves typescripts language server enables supports file renames and auto imports
+  use({
+    "jose-elias-alvarez/nvim-lsp-ts-utils",
+    ft = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+    after = "nvim-lspconfig",
+    requires = { "nvim-lua/plenary.nvim" },
+  })
+
+  -- File tree
   use({
     "kyazdani42/nvim-tree.lua",
+    event = "BufEnter",
+    keys = { { "n", "<C-e>" }, { "n", "<leader>n" } },
     config = function()
       require("ld.plugins.nvim-tree")
     end,
   })
 
-  use({ "hrsh7th/vim-vsnip", requires = {
-    "rafamadriz/friendly-snippets",
-  } })
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/cmp-path",
-      "kristijanhusak/vim-dadbod-completion",
-    },
-    config = function()
-      require("ld.plugins.nvim-cmp")
-      require("ld.plugins.vim-vsnip")
-    end,
-  })
+  -- Lua language server
   use({
     "sumneko/lua-language-server",
+    opt = true,
     run = global.is_unix and [[cd 3rd/luamake && compile/install.sh && cd ../.. && ./3rd/luamake/luamake rebuild]]
       or [[cd 3rd\luamake & compile\install.bat & cd ..\.. & 3rd\luamake\luamake.exe rebuild]],
   })
+
+  -- Org mode
   use({
     "kristijanhusak/orgmode.nvim",
     requires = {
-      "lukas-reineke/headlines.nvim",
-      "akinsho/org-bullets.nvim",
+      { "lukas-reineke/headlines.nvim" },
+      { "akinsho/org-bullets.nvim" },
     },
     config = function()
       require("ld.plugins.orgmode")
