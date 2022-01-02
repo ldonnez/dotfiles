8a9b3ec10a35a88a320e815405d6d56278ac1cfd
Lenny <lenny.donnez@gmail.com>
2022-01-02 20:52:03 +0100

    Neovim 0.6 diagnostic highlight has been added to nord-vim

--- .config/nvim/lua/ld/plugins/nord.lua
+++ .config/nvim/lua/ld/plugins/nord.lua
@@ -1,21 +1,3 @@
--- Custom higlight Diagnostics until nvim 0.6 improvements are merged in the nord-vim repo
-vim.cmd([[ autocmd ColorScheme nord highlight DiagnosticWarn ctermfg=3 guifg=#EBCB8B ]])
-vim.cmd([[ autocmd ColorScheme nord highlight DiagnosticError ctermfg=1 guifg=#BF616A ]])
-vim.cmd([[ autocmd ColorScheme nord highlight DiagnosticInfo ctermfg=6 guifg=#88C0D0 ]])
-vim.cmd([[ autocmd ColorScheme nord highlight DiagnosticHint ctermfg=12 guifg=#5E81AC ]])
-vim.cmd(
-  [[ autocmd ColorScheme nord highlight DiagnosticUnderlineWarn ctermfg=3 guifg=#EBCB8B gui=undercurl cterm=underline ]]
-)
-vim.cmd(
-  [[ autocmd ColorScheme nord highlight DiagnosticUnderlineError ctermfg=1 guifg=#BF616A gui=undercurl cterm=underline ]]
-)
-vim.cmd(
-  [[ autocmd ColorScheme nord highlight DiagnosticUnderlineInfo ctermfg=6 guifg=#88C0D0 gui=undercurl cterm=underline ]]
-)
-vim.cmd(
-  [[ autocmd ColorScheme nord highlight DiagnosticUnderlineHint ctermfg=12 guifg=#5E81AC gui=undercurl cterm=underline ]]
-)
-
 vim.cmd([[
 	silent! colorscheme nord
 ]])
