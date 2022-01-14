216cd77793724f3e934b336f1a61b2e2bb2d8f8f
Lenny <lenny.donnez@gmail.com>
2022-01-14 12:51:25 +0100

    Formatting

--- .config/nvim/lua/ld/lsp/utils.lua
+++ .config/nvim/lua/ld/lsp/utils.lua
@@ -25,8 +25,12 @@ function utils.cmp_capababilities()
 end
 
 function utils.on_attach(_, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
 
   --Enable completion triggered by <c-x><c-o>
   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
