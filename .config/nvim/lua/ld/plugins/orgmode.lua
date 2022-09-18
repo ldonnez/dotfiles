710eb70cb6cbe372f175a02735268634bf33c21b
Lenny <lenny.donnez@gmail.com>
2022-09-18 13:50:52 +0200

    Improve org mode capturing

--- .config/nvim/lua/ld/plugins/orgmode.lua
+++ .config/nvim/lua/ld/plugins/orgmode.lua
@@ -1,26 +1,70 @@
-local orgDestination = "~/SynologyDrive/org"
-
+local orgDestination = "~/org"
 
 require("orgmode").setup_ts_grammar()
 require("orgmode").setup({
   org_agenda_files = { orgDestination .. "/*", orgDestination .. "/**/*" },
   org_default_notes_file = orgDestination .. "/refile.org",
   org_agenda_templates = {
-    t = { description = "Task", template = "\n* TODO %?\n %u", target = orgDestination .. "/todos.org" },
+    t = "Todo",
+    tp = {
+      description = "Personal",
+      template = "* TODO %?\n %u",
+      target = orgDestination .. "/todos.org",
+      headline = "Personal",
+    },
+    tw = {
+      description = "Work",
+      template = "* TODO %?\n %u",
+      target = orgDestination .. "/todos.org",
+      headline = "Work",
+    },
     j = {
       description = "Journal",
-      template = "\n** %<%Y-%m-%d> %<%A>\n**** %U\n\n   %?",
+      template = "* %<%Y-%m-%d> %<%A>\n**** %U\n\n   %?",
       target = orgDestination .. "/journal.org",
     },
     b = {
       description = "Braindump",
-      template = "** %U\n <%a>\n\n %?",
+      template = "* %U\n <%a>\n\n %?",
       target = orgDestination .. "/braindump.org",
     },
-    s = {
-      description = "Shopping list",
-      template = " + [ ] %?",
+    s = "Shopping",
+    sd = {
+      description = "Delhaize",
+      template = "+ [ ] %?",
       target = orgDestination .. "/shopping_list.org",
+      headline = "Delhaize",
+    },
+    si = {
+      description = "Ikea",
+      template = "+ [ ] %?",
+      target = orgDestination .. "/shopping_list.org",
+      headline = "Ikea",
+    },
+    sa = {
+      description = "Aldi",
+      template = "+ [ ] %?",
+      target = orgDestination .. "/shopping_list.org",
+      headline = "Aldi",
+    },
+    sl = {
+      description = "Lidl",
+      template = "+ [ ] %?",
+      target = orgDestination .. "/shopping_list.org",
+      headline = "Lidl",
+    },
+    z = "Books",
+    zw = {
+      description = "Wishlist",
+      template = "  + [ ] %?",
+      target = orgDestination .. "/books.org",
+      headline = "Books",
+    },
+    za = {
+      description = "Authors to check",
+      template = "  - %?",
+      target = orgDestination .. "/books.org",
+      headline = "Authors",
     },
   },
   mappings = {
