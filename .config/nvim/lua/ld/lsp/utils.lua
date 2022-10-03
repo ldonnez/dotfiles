d76217531ebacdd96b3d8d2f805d297f0b200bb1
Lenny <lenny.donnez@gmail.com>
2022-10-03 11:10:19 +0200

    Comply with new api neovim 0.8

--- .config/nvim/lua/ld/lsp/utils.lua
+++ .config/nvim/lua/ld/lsp/utils.lua
@@ -55,7 +55,7 @@ function utils.on_attach(_, bufnr)
   buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
   buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
   buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async = true})<CR>", opts)
 end
 
 return utils
