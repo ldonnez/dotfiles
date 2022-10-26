a06a874b7ee4e5b5d73366e0ebaa73542bd3d19e
Lenny <lenny.donnez@gmail.com>
2022-10-26 12:30:24 +0200

    update_capabilities -> default_capabilities
    cmp_nvim_lsp.update_capabilities is deprecedated

--- .config/nvim/lua/ld/lsp/utils.lua
+++ .config/nvim/lua/ld/lsp/utils.lua
@@ -20,7 +20,7 @@ end
 
 function utils.cmp_capababilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
   return capabilities
 end
 
