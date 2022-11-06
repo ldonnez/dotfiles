b9b8705e27993fc23350ffa029e25b042ee5759e
Lenny <lenny.donnez@gmail.com>
2022-11-06 23:47:50 +0100

    Update catppuccin -> 0.2.6

--- .config/nvim/lua/ld/plugins/init.lua
+++ .config/nvim/lua/ld/plugins/init.lua
@@ -15,7 +15,7 @@ require("packer").startup(function()
   use({
     "catppuccin/nvim",
     as = "catppuccin",
-    tag = "v0.2.5",
+    tag = "v0.2.6",
     config = function()
       require("ld.plugins.catppuccin")
     end,
