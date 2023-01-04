4323c7792e15aa48dd85a6f0f601f00cc3b7f1e3
Lenny <lenny.donnez@gmail.com>
2023-01-04 23:00:08 +0100

    Return simple table
    Unnecessary to call function

--- .config/nvim/lua/config/global.lua
+++ .config/nvim/lua/config/global.lua
@@ -1,21 +1,16 @@
-local global = {}
 local home = vim.fn.getenv("HOME")
 local config = vim.fn.stdpath("config")
 local data = vim.fn.stdpath("data")
 
-function global:load_variables()
-  self.home_dir = home
-  self.config_dir = config
-  self.backup_dir = config .. "/backup"
-  self.undo_dir = config .. "/undo"
-  self.swap_dir = config .. "/swap"
-  self.data = data
-  self.plugin_installation_path = data .. "/lazy"
-  self.is_windows = vim.fn.has("win32") == 1
-  self.is_unix = vim.fn.has("unix") == 1
-  self.is_mac = vim.fn.has("mac") == 1
-end
-
-global:load_variables()
-
-return global
+return {
+  home_dir = home,
+  config_dir = config,
+  backup_dir = config .. "/backup",
+  undo_dir = config .. "/undo",
+  swap_dir = config .. "/swap",
+  data = data,
+  plugin_installation_path = data .. "/lazy",
+  is_windows = vim.fn.has("win32") == 1,
+  is_unix = vim.fn.has("unix") == 1,
+  is_mac = vim.fn.has("mac") == 1,
+}
