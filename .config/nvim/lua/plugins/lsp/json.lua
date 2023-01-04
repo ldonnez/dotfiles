538d8e592c8b0b9b3f33ba10a8d250c49e70fa03
Lenny <lenny.donnez@gmail.com>
2023-01-04 05:48:54 +0100

    Remove deprecated format

--- .config/nvim/lua/plugins/lsp/json.lua
+++ .config/nvim/lua/plugins/lsp/json.lua
@@ -5,11 +5,4 @@ local on_attach = require("plugins.lsp.utils").on_attach
 lspconfig.jsonls.setup({
   capabilities = capabilities,
   on_attach = on_attach,
-  commands = {
-    Format = {
-      function()
-        vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-      end,
-    },
-  },
 })
