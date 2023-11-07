cabc3471c0d157aeb39e0dbc8046bd25754c7a90
Lenny <lenny.donnez@gmail.com>
2023-11-07 20:03:31 +0100

    Don't defer autocmd's based on FileType
    Defering based on filetype won't work

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -2,14 +2,10 @@ local global = require("global")
 
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
+local fn = vim.fn
 
 vim.defer_fn(function()
   autocmd({ "TermOpen" }, { pattern = "*", command = "setlocal nonumber list" })
-
-  autocmd({ "FileType" }, {
-    pattern = "mail",
-    command = "set noautoindent | setlocal nosmartindent | setlocal textwidth=72 | setlocal nonumber",
-  })
 
   if global.is_wsl then
     autocmd("TextYankPost", {
@@ -17,8 +13,13 @@ vim.defer_fn(function()
       group = augroup("Yank", { clear = true }),
 
       callback = function()
-        vim.fn.system("clip.exe", vim.fn.getreg('"'))
+        fn.system("clip.exe", fn.getreg('"'))
       end,
     })
   end
 end, 10)
+
+autocmd({ "FileType" }, {
+  pattern = "mail",
+  command = "set noautoindent | setlocal nosmartindent | setlocal textwidth=72 | setlocal nonumber",
+})
