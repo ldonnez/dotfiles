468002c3eb6487c374706fc8f7609f5ac562f94e
Lenny <lenny.donnez@gmail.com>
2023-11-14 21:19:42 +0100

    <leader>y -> copy current filepath to clipboard

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -6,6 +6,14 @@ g.mapleader = ","
 keymap.set("i", "jj", "<esc>", { silent = true, desc = "Remap esc" })
 keymap.set("n", "j", "gj", { silent = true, desc = "Remap esc" })
 keymap.set("n", "k", "gk", { silent = true, desc = "Remap esc" })
+keymap.set("n", "<leader>y", function()
+  local path = vim.fn.expand("%")
+  local relative_path = vim.fn.fnamemodify(path, ":~:.")
+
+  vim.fn.setreg("+", relative_path)
+
+  print("Copied to clipboard: " .. relative_path)
+end, { silent = true, desc = "Copy current file path to clipboard" })
 
 keymap.set("n", "<leader><space>", [[:%s/\s\+$//<CR>]], { silent = true, desc = "Remove whitespace" })
 
