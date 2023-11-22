ad7d319a77c15ad144736ec518cbb5b80d9c55f8
Lenny <lenny.donnez@gmail.com>
2023-11-22 09:32:47 +0100

    Add mappings to go to next/previous buffer

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -6,6 +6,10 @@ g.mapleader = ","
 keymap.set("i", "jj", "<esc>", { silent = true, desc = "Remap esc" })
 keymap.set("n", "j", "gj", { silent = true, desc = "Remap esc" })
 keymap.set("n", "k", "gk", { silent = true, desc = "Remap esc" })
+
+keymap.set("n", "<S-l>", ":bnext<CR>", { silent = true, desc = "Go to previous buffer" })
+keymap.set("n", "<S-h>", ":bprevious<CR>", { silent = true, desc = "Go to next buffer" })
+
 keymap.set("n", "<leader>y", function()
   local path = vim.fn.expand("%")
   local relative_path = vim.fn.fnamemodify(path, ":~:.")
