963ac800e1cc1927a8f17595e6cac0e8bb986218
Lenny <lenny.donnez@gmail.com>
2023-11-22 20:11:13 +0100

    Update rest.nvim
    JSON responses are correctly formatted now.

--- .config/nvim/lua/plugins/rest.lua
+++ .config/nvim/lua/plugins/rest.lua
@@ -1,6 +1,5 @@
 return {
   "rest-nvim/rest.nvim",
-  commit = "8b62563cfb19ffe939a260504944c5975796a682",
   ft = { "http", "json" },
   keys = {
     { "<leader>rr", "<Plug>RestNvim <CR>", desc = "Run HTTP Request under cursor" },
