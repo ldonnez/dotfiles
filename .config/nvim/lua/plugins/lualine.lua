e4951a9c0e749ce8694bc4992e1531150ec207ce
Lenny <lenny.donnez@gmail.com>
2024-01-09 10:34:27 +0100

    Enable oil extension
    Shows path in status line when opening oil directory tree.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -29,7 +29,7 @@ function M.config()
         },
       },
     },
-    extensions = { "fugitive" },
+    extensions = { "fugitive", "oil" },
   })
 
   -- lualine overrides this option; makes sure tabline only shows when there are atleast 2 tabpages
