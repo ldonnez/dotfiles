a727e6ca4b0cf9ac727b26154ee7cd3ac4ccb4f7
Lenny <lenny.donnez@gmail.com>
2024-02-26 09:59:58 +0100

    Add sqlfmt for formatting sql

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -14,6 +14,7 @@ return {
       scss = { { "prettierd", "prettier" } },
       css = { { "prettierd", "prettier" } },
       markdown = { { "prettierd", "prettier" } },
+      sql = { "sqlfmt" },
     },
   },
   keys = {
