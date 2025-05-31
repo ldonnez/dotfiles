c6b317f96c056271c4565d77739f0f79c513190e
Lenny <lenny.donnez@gmail.com>
2025-05-31 19:05:45 +0200

    Simplify colors override blink-cmp
    Due to improvements of new version of catppuccin.

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -8,12 +8,7 @@ return {
     -- Override blink colors until color issues fixed in catppuccin.
     custom_highlights = function(colors)
       return {
-        BlinkCmpKind = { fg = colors.blue },
         BlinkCmpMenu = { fg = colors.text },
-        BlinkCmpMenuBorder = { fg = colors.blue },
-        BlinkCmpDocBorder = { fg = colors.blue },
-        BlinkCmpSignatureHelpActiveParameter = { fg = colors.mauve },
-        BlinkCmpSignatureHelpBorder = { fg = colors.blue },
       }
     end,
     integrations = {
