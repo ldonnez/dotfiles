362dccae161617be9657e89e2106e7416a7e16df
Lenny <lenny.donnez@gmail.com>
2025-08-21 21:46:50 +0200

    Move from rest.nvim to kulala.nvim

---
+++ .config/nvim/lua/plugins/kulala.lua
@@ -0,0 +1,10 @@
+return {
+  "mistweaverco/kulala.nvim",
+  version = "*",
+  ft = { "http", "rest" },
+  opts = {
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>R",
+    kulala_keymaps_prefix = "",
+  },
+}
