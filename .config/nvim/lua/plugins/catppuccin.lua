dfdecb002e5c4a8807a0c92bb85a8a5c3fa4d7a4
Lenny <lenny.donnez@gmail.com>
2026-03-30 19:30:49 +0200

    Correctly load catppuccin theme

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,17 +4,18 @@ return {
   version = "*",
   lazy = false,
   priority = 1000,
-  opts = {
-    flavour = "frappe",
-    -- Override blink background text to make it the same as vim background.
-    custom_highlights = function(colors)
-      return {
-        BlinkCmpMenu = { fg = colors.text },
-      }
-    end,
-    auto_integrations = true,
-  },
-  init = function()
+  config = function()
+    require("catppuccin").setup({
+      flavour = "frappe",
+      -- Override blink background text to make it the same as vim background.
+      custom_highlights = function(colors)
+        return {
+          BlinkCmpMenu = { fg = colors.text },
+        }
+      end,
+      auto_integrations = true,
+    })
+
     vim.cmd.colorscheme("catppuccin")
   end,
 }
