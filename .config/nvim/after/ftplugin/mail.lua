5064eca06ebe2d69b997435c0856e8df9a741652
Lenny <lenny.donnez@gmail.com>
2026-04-11 23:57:45 +0200

    Refactor config to vim.pack

---
+++ .config/nvim/after/ftplugin/mail.lua
@@ -0,0 +1,7 @@
+vim.bo.autoindent = false
+vim.bo.smartindent = false
+vim.bo.textwidth = 72
+vim.opt_local.number = false
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en,nl"
+vim.opt_local.spelloptions = "camel"
