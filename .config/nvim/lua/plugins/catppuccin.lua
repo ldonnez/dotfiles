fd5edfb076e6942a6d49ff507f6935a50ff38e65
Lenny <lenny.donnez@gmail.com>
2026-04-21 17:28:50 +0200

    Remove unnecessary setup
    Not necessary to call setup anymore for default settings since
    catppuccin v2.0

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -5,17 +5,6 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavour = "frappe",
-      -- Override blink background text to make it the same as vim background.
-      custom_highlights = function(colors)
-        return {
-          BlinkCmpMenu = { fg = colors.text },
-        }
-      end,
-      auto_integrations = true,
-    })
-
-    vim.cmd.colorscheme("catppuccin")
+    vim.cmd.colorscheme("catppuccin-frappe")
   end,
 }
