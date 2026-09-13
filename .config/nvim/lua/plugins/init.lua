b004c4ad16fb90002742b20fa5786dfe1c32f7ca
Lenny <lenny.donnez@gmail.com>
2026-09-13 20:41:09 +0200

    Declare filetype parsers
    To ensure we can format encrypted files inside buffers we have to
    declare the parsers per filetype.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -180,18 +180,25 @@ pack.load({
   },
   setup = function()
     require("conform").setup({
+      formatters = {
+        prettier = {
+          options = {
+            ft_parsers = { markdown = "markdown", json = "json", yaml = "yaml" },
+          },
+        },
+      },
       formatters_by_ft = {
         lua = { "stylua" },
         typescript = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-        yaml = { "prettierd", "prettier", stop_after_first = true },
-        json = { "prettierd", "prettier", stop_after_first = true },
-        html = { "prettierd", "prettier", stop_after_first = true },
+        yaml = { "prettier", stop_after_first = true },
+        json = { "prettier", stop_after_first = true },
+        html = { "prettier", stop_after_first = true },
         scss = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", "prettier", stop_after_first = true },
-        markdown = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettier", stop_after_first = true },
         sql = { "sqlfmt" },
         terraform = { "terraform_fmt" },
         bash = { "shfmt" },
